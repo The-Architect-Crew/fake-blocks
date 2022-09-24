4fdda4de484d0dd37b8179e3c8421253eb6b81f4
neinwhal <3191240+neinwhal@users.noreply.github.com>
2022-09-25 00:55:17 +0800

    Remove duplicate alias

--- default2/alias.lua
+++ default2/alias.lua
@@ -1,10 +1,3 @@
-minetest.register_alias("default:torch", "blocks:torch")
-minetest.register_alias("default:torch_wall", "blocks:torch_wall")
-minetest.register_alias("default:torch_ceiling", "blocks:torch_ceiling")
-minetest.register_alias("default:mese_post_light", "blocks:mese_post_light")
-minetest.register_alias("default:junglegrass", "flora:junglegrass_1")
-minetest.register_alias("flowers:tulip", "flora:tulip")
-
 minetest.register_alias("arrow_signs:wall", "blocks:arrowsign_wood")
 minetest.register_alias("arrow_signs:steel", "blocks:arrowsign_steel")
 minetest.register_alias("locked_sign:sign_wall_locked", "blocks:sign_wood")
@@ -13,11 +6,7 @@ minetest.register_alias("itemframes:frame", "blocks:itemframe_steel")
 
 minetest.register_alias("moreores:mineral_tin", "blocks:stone_with_tin")
 minetest.register_alias("moreores:tin_block", "blocks:tinblock")
-minetest.register_alias("default:stone_with_tin", "blocks:stone_with_tin")
-minetest.register_alias("default:tinblock", "blocks:tinblock")
-
 minetest.register_alias("moreores:mineral_silver", "blocks:stone_with_silver")
 minetest.register_alias("moreores:silver_block", "blocks:silverblock")
-
 minetest.register_alias("moreores:mineral_mithril", "blocks:stone_with_mithril")
 minetest.register_alias("moreores:mithril_block", "blocks:mithrilblock")
